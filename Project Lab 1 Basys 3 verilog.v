06b36a89ab0f187073b55ba4def1cd59a32d8480
EE-Mike <89888180+EE-Mike@users.noreply.github.com>
2021-09-02 17:47:26 -0500

    Add files via upload

--- Project Lab 1 Basys 3 verilog.v
+++ Project Lab 1 Basys 3 verilog.v
@@ -9,28 +9,51 @@
 //and forward direction. Switches 4-7 will be speed and rearward direction.
 //The choice of a switch with active high will result in the appropriate duty
 //cycle as well as the appropriate output values to the L298 Bridge to indicate 
-//motor selection as well as motor speed and direction.
+//motor selection as well as motor speed and direction. Version 1.0
 
 module Basys3 (v17,PWR);
 
-//Inputs
+//Inputs/switches 0-7 respectively
 input v17;
 input v16;
 input w16;
 input w17;
-input [7:0] pulse_width;
-input w5;
+input w15;
+input v15;
+input w14;
+input w13;
 
-//Output
+//pulse-width modulation variables
+input [7:0] pulse_width;
+input w5; //100Mhz Oscillator BASYS_3
+
+//Outputs to Motor-A
+output k17;
+output m18;
+output n17;
+output p18;
+
 output PWR;
+output GND;
 
 //Wires
+wire k17;
+wire m18;
+wire n17;
+wire p18;
 wire PWR;
+wire GND;
 
 //registers
 reg v17;
+reg v16;
+reg w16;
+reg w17;
+reg w15;
+reg v15;
+reg w14;
+reg w13;
 reg [7:0] counter = 0;
-reg PWR;
 
 //pulse_width initialization *stop*
 pulse_width = 0;
@@ -43,14 +66,24 @@ always (@posedge w5) begin
     end
     counter <= counter+1;
     //changes pulse width depending on the value of pulse_width
-    //which is obtained by polling switches 0-3 for 25%/50%/75%/100%
+    //which is obtained by polling switches 0-7 for 25%/50%/75%/100%
+    //duty cycle.
 
+    //Switches 0-3 are forward direction; switches 4-7 are reverse direction
+    //NEEDS PIN ASSIGNMENTS FOR MOTOR DIRECTION AND MOTOR SELECTION//
     begin
-        case(v17,v16,w16,w17)
-        v17 = 1: pulse_width <= 100; //100% duty @ 3.3V
-        v16 = 1; pulse_width <= 75;  //75% duty @ 2.475V
-        w16 = 1; pulse_width <= 50;  //50% duty @ 1.65V
-        w17 = 1; pulse_width <= 25;  //25% duty @ 0.825V
+        case(v17 or v16 or w16 or w17 or w15 or v15 or w14 or w13 or pulse_width)
+
+            v17 = 1: pulse_width <= 100; //100% duty @ 3.3V
+            v16 = 1; pulse_width <= 75;  //75% duty @ 2.475V 
+            w16 = 1; pulse_width <= 50;  //50% duty @ 1.65V  
+            w17 = 1; pulse_width <= 25;  //25% duty @ 0.825V 
+
+            w15 = 1; pulse_width <= 100; //100% duty @ 3.3V
+            v15 = 1; pulse_width <= 75;  //75% duty @ 2.475V
+            w14 = 1; pulse_width <= 50;  //50% duty @ 1.65V
+            w13 = 1; pulse_width <= 25;  //25% duty @ 0.825V
+
         default pulse_width <= 0;    //0% duty @ 0.0V
     end
     
