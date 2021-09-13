050c85c5dd61a0a4eb1c7f3569df095512d5820a
EE-Mike <89888180+EE-Mike@users.noreply.github.com>
2021-09-12 22:52:46 -0500

    Add files via upload

--- Project Lab 1 Basys 3 verilog.v
+++ Project Lab 1 Basys 3 verilog.v
@@ -11,7 +11,7 @@
 //control and forward direction. Switches 4-7 will be speed control and rearward| 
 //direction. The choice of a switch enables active high and will result in the  |
 //appropriate duty cycle as well as the appropriate output values to the L298   |
-//bridge to indicate motor selection as well. Version 1.7                       |
+//bridge to indicate motor selection as well. Version 1.8                       |
 //-------------------------------------------------------------------------------
 
 module Basys3 (clk,sw0,sw1,sw2,sw3,sw4,sw5,sw6,sw7,JC0,JC1,JC2);
@@ -51,19 +51,31 @@ output reg JC2; // PWM_OUT(Speed Control) Pin:N17
 //Pulse Width Modulation variable initialization
 reg [3:0] counter_1 = 0;
 reg [3:0] pulse_width = 0;
+//reg [25:0] counter = 0;
+//reg [3:0] clk_out = 0;
 
 //Direction enable
 reg enable_dir = 1;
+
+/*always @(posedge clk) begin
+    counter <= counter +1;
+    if(counter == 2_500_000) begin //40 Hz Clock or 25ms Period T = 1/F.
+        clk_out <= 1'b1;
+        counter <= 0;
+    end else begin
+        clk_out <= 1'b0;  
+    end 
+
+end*/
 
 //--------------------------------------------
 //Pulse_width modulation                     |
 //--------------------------------------------
 always @(posedge clk) begin
     counter_1 <= counter_1+1;
-    if(counter_1 >=9) begin
+    if(counter_1 >= 9) begin
         counter_1 <= 0;
     end
-
     if (counter_1 < pulse_width) begin
         JC2 = 1'b1;
     end else begin
@@ -82,9 +94,8 @@ end
 //switches 4-7 are reverse direction @ variable pulse_width.    |
 //---------------------------------------------------------------
 always @(posedge clk) begin
-        
-    pulse_width <= 0; //Reset Logic low for a consecutive switch press after a logic high assignment    
     
+    pulse_width <= 0;
     if (sw0 == 1) begin
         pulse_width <= 10;
         enable_dir <= 1; 
@@ -106,28 +117,28 @@ always @(posedge clk) begin
     end
     
     if (sw4 == 1) begin
-        pulse_width <= 10;
+        pulse_width <= 40;
         enable_dir <= 0;
     end
 
     if (sw5 == 1) begin
-        pulse_width <= 7;
+        pulse_width <= 30;
         enable_dir <= 0;
     end
 
     if (sw6 == 1) begin
-        pulse_width <= 5;
+        pulse_width <= 20;
         enable_dir <= 0;
     end
 
     if (sw7 == 1) begin
-        pulse_width <= 3;
-        enable_dir <= 1;
+        pulse_width <= 10;
+        enable_dir <= 0;
     end
 
-    if(enable_dir ==1) begin
+    if(enable_dir == 1) begin
         JC0 = 1'b1; //Forward Direction
-        JC0 = 1'b0; //Ditto
+        JC1 = 1'b0; //Ditto
     end else begin
         JC0 = 1'b0; //Reverse Direction
         JC1 = 1'b1; //Ditto
