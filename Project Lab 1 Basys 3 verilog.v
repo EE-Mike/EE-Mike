4aebfc81b5e4adde06e24659a9b3b0510eff932f
EE-Mike <89888180+EE-Mike@users.noreply.github.com>
2021-09-16 11:56:34 -0500

    Add files via upload

--- Project Lab 1 Basys 3 verilog.v
+++ Project Lab 1 Basys 3 verilog.v
@@ -14,7 +14,7 @@
 //bridge to indicate motor selection as well. Version 1.9                       |
 //-------------------------------------------------------------------------------
 
-module Basys3 (clk,sw0,sw1,sw2,sw3,sw4,sw5,sw6,sw7,JC0,JC1,JC2,JC7,JC8,JC9);
+module Basys3 (clk,sw0,sw1,sw2,sw3,sw4,sw5,sw6,sw7,JC0,JC1,JC2,JC7,JC8,JC9,currentSenseA,currentSenseB);
 //----------------------------------------------------------------------------
 //Inputs                                                                     |
 //----------------------------------------------------------------------------
@@ -42,8 +42,10 @@ output reg JC2; // PWM_OUT(Speed Control) Pin:N17
 //Outputs to Motor-B
 output reg JC7; // Direction     Control  Pin:L17
 output reg JC8; // Direction     Control  Pin:M19
-output reg JC9; // PWM_OUT(Speed Control) Pin:P17
+output reg JC9; // PWM_OUT(Speed Control) Pin:N18
 
+output reg currentSenseA;    //Current Protect for Motor A
+output reg currentSenseB;   //Current Protect for Motor B
 //----------------------------------------------------------------------------
 //Registers                                                                  |
 //----------------------------------------------------------------------------
@@ -55,13 +57,19 @@ reg [3:0] pulse_width = 0;
 //Direction enable
 reg enable_dir = 1;
 
+//Overcurrent Protection 
+reg turnOff;
+reg [20:0] counterCurrentA,counterCurrentB,counterCurrent_limit;
+
 //---------------------------------------------------------------------------
 //Pulse_width modulation                                                    |
 //---------------------------------------------------------------------------
 always @(posedge clk) begin
-    counter_1 <= counter_1+1;
-    if(counter_1 >= 9) begin // 40khz carrier signal
+    
+    if(counter_1 >= 100) begin // 10Mhz carrier signal
         counter_1 <= 0;
+    end else begin
+        counter_1 <= counter_1+1; 
     end
     if (counter_1 < pulse_width) begin
         JC2 = 1'b1;
@@ -74,7 +82,7 @@ always @(posedge clk) begin
 end
 
 //-------------------------------------------------------------------------
-//Pulse_Width and Direction Selection                                     |
+//Pulse_Width and Direction Selection (I and II)                          |
 //-------------------------------------------------------------------------
 //Changes the pulse width depending on the value of pulse_width.          |
 //which is obtained by polling switches 0-7.                              |
@@ -83,60 +91,121 @@ end
 //-------------------------------------------------------------------------
 always @(posedge clk) begin
     
+    if (turnOff == 1) begin 
+
     pulse_width <= 0;
     if (sw0 == 1) begin
-        pulse_width <= 10;
-        enable_dir <= 1; 
+
+        pulse_width <= 100;
+        enable_dir <= 1;
+
     end 
 
     if (sw1 == 1) begin
-        pulse_width <= 7;
+
+        pulse_width <= 75;
         enable_dir <= 1;
+
     end
 
     if (sw2 == 1) begin
-        pulse_width <= 5;
+
+        pulse_width <= 50;
         enable_dir <= 1;
-    end
+
+    end 
 
     if (sw3 == 1) begin
-        pulse_width <= 3;
+
+        pulse_width <= 25;
         enable_dir <= 1;
+
     end
     
     if (sw4 == 1) begin
-        pulse_width <= 10;
+
+        pulse_width <= 100;
         enable_dir <= 0;
-    end
+    
+    end 
 
     if (sw5 == 1) begin
-        pulse_width <= 7;
+
+        pulse_width <= 75;
         enable_dir <= 0;
-    end
+
+    end 
 
     if (sw6 == 1) begin
-        pulse_width <= 5;
+
+        pulse_width <= 50;
         enable_dir <= 0;
-    end
+
+    end 
 
     if (sw7 == 1) begin
-        pulse_width <= 3;
+
+        pulse_width <= 25;
         enable_dir <= 0;
+    
     end
 
+    //---------------------------|
+    //Direction Assignment Block |
+    //---------------------------|
     if(enable_dir == 1) begin
         JC0 = 1'b1; //Forward Direction
         JC1 = 1'b0; //Ditto
-        JC7 = 1'b1; //Ditto
-        JC8 = 1'b0; //Ditto
-    end else begin
-        JC0 = 1'b0; //Reverse Direction
-        JC1 = 1'b1; //Ditto
         JC7 = 1'b0; //Ditto
         JC8 = 1'b1; //Ditto
+    end 
+    else begin
+        JC0 = 1'b0; //Reverse Direction
+        JC1 = 1'b1; //Ditto
+        JC7 = 1'b1; //Ditto
+        JC8 = 1'b0; //Ditto
 
     end
+    end
+end
+
+//----------------------------|
+//Over Current Implimentation |
+//----------------------------|
+//----------------------------------------------|
+//When the current to either motor exceeds 1A   |
+//ENA and ENB are disabled to protect the motor |
+//from over surge                               |
+//----------------------------------------------|
+initial begin
+
+    counterCurrentA = 1'd0;
+    counterCurrentB = 1'd0;
+    counterCurrent_limit = 4'd9;
+    turnOff = 1;
 
 end
+
+always @ (posedge clk) begin
+
+   if (currentSenseA == 1)
+        counterCurrentA <= counterCurrentA + 1;
+   else if (currentSenseA == 0) begin
+        counterCurrentA <= 0;
+   end
+
+   if (counterCurrentA == counterCurrent_limit)
+        turnOff <= 0;
+
+   if (currentSenseB == 1)
+        counterCurrentB <= counterCurrentB + 1;
+   else if (currentSenseB == 0) begin
+        counterCurrentB <= 0;
+   end
+
+   if (counterCurrentB == counterCurrent_limit)
+        turnOff <= 0;
+
+   end 
 
 endmodule
